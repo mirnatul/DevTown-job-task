bebc1d89a28c4489d97bcbfa4611c319bff6e964
Li Pon <mirnatul@gmail.com>
2023-11-02 07:14:57 +0600

    tailwind and daisyui added and routing set up completed

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from './Layout/Main';
+import Home from './pages/home/Home';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Project Set!!</div>,
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      }
+    ]
   },
 ]);
 
